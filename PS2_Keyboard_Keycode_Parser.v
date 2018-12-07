e4a6c1184c706ac8ea7e6f0a3765190998782a70
AdamC19 <cordingleyadam19+github@gmail.com>
2018-12-07 00:06:17 -0500

    Add code for PS2 Keyboard Keycode parser, missing S5 stuff

--- PS2_Keyboard_Keycode_Parser.v
+++ PS2_Keyboard_Keycode_Parser.v
@@ -61,5 +61,88 @@ module PS2_Keyboard_Keycode_Parser
 	//
 
 	// !! Lab 5: Add the PS2 Keyboard Keycode Parser State Machine here !!
+	// state machine variable definition
+	reg [5:0] state;
+	localparam [5:0]
+		S0 = 6'b000001,
+		S1 = 6'b000010,
+		S2 = 6'b000100,
+		S3 = 6'b001000,
+		S4 = 6'b010000,
+		S5 = 6'b100000;
+	
+	//
+	// STATE MACHINE PROCESS
+	//
+	always @(posedge CLK, posedge RESET) begin
+		if(RESET) begin
+			state 				<= S0;
+			KEY_PRESSED_MAKE 	<= 1'b0;
+			KEY_PRESSED_BREAK <= 1'b0;
+			KEY_PRESSED_DATA 	<= 8h'00;
+			key_release_flag 	<= 1'b0;
+		end
+		
+		else begin
+		
+			case(state)
+				S0:
+				begin
+					// wait for Key Ready Signal
+					if(KEY_READY)
+						state <= S1;
+				end
+				
+				S1:
+				begin
+					case(status_flag)
+						2'h0: key_release_flag <= key_release_flag;
+						2'h1: key_release_flag <= 1'b0;
+						2'h2: key_release_flag <= 1'b1;
+						2'h3: key_release_flag <= key_release_flag;
+					endcase
+					
+					if(status_flag != 2h'0) begin
+						state <= S0;
+					end
+					else begin
+						state <= S2;
+					end
+					
+				end
+				
+				S2:
+				begin
+					KEY_PRESSED_DATA <= KEY_DATA; 	// output Key Data
+					if(key_release_flag)
+						state <= S4;
+					else
+						state <= S3;
+				end
+				
+				S3:
+				begin
+					KEY_PRESSED_MAKE 		<= 1'b1;
+					KEY_PRESSED_BREAK 	<= 1'b0;
+					state <= S5;
+				end
+				
+				S4:
+				begin
+					KEY_PRESSED_MAKE 		<= 1'b0;
+					KEY_PRESSED_BREAK 	<= 1'b1;
+					state <= S5;
+				end
+				
+				S5:
+				begin
+					// CLEAR CONTROL SIGNALS ??
+					state <= S0;
+				end
+				
+			endcase
+		
+		end
+	end
 	
 endmodule
